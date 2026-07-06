461d0deb4d03f81453b257466997885ebabf5038
Zhihan Liu <2LIUZ98@solent.ac.uk>
2026-07-06 20:00:57 +0100

    FRONTNED: ABOUT

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import HomePage from "./views/Home";
 import AirportTransfer from "./views/Airport";
+import About from "./views/About";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/airport-transfer" element={<AirportTransfer />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 }
