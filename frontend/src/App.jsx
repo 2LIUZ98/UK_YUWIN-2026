582c6404897e380dccc241ce400f9e17bccf28f7
Zhihan Liu <2LIUZ98@solent.ac.uk>
2026-07-06 19:32:45 +0100

    FRONTNED:  AIRPORT

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import HomePage from "./views/Home";
+import { Routes, Route } from 'react-router-dom';
 
-// Main application component defining routing structure
+import HomePage from "./views/Home";
+import AirportTransfer from "./views/Airport";
+
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/airport-transfer" element={<AirportTransfer />} />
     </Routes>
   );
 }
