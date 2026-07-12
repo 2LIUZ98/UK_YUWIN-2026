21fdd34b2a82358d5a1ec5861f2e600a0beb0368
Zhihan Liu <2LIUZ98@solent.ac.uk>
2026-07-12 18:18:29 +0100

    BACKEND: DOTNEV/CONFIG

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 import express from "express";
 import cors from "cors";
 import ViteExpress from "vite-express";
@@ -7,14 +9,18 @@ import vehiclesRouter from "./routes/vehicles.mjs";
 import inquiriesRouter from "./routes/inquiries.mjs";
 import categoriesRouter from "./routes/categories.mjs";
 
+
 // Initialise Express application
 const app = express();
+
 
 // Enable Cross-Origin Resource Sharing (CORS)
 app.use(cors());
 
+
 // Parse incoming JSON requests
 app.use(express.json());
+
 
 // Register API routes
 app.use("/routes", routesRouter);
@@ -22,7 +28,10 @@ app.use("/vehicles", vehiclesRouter);
 app.use("/inquiries", inquiriesRouter);
 app.use("/categories", categoriesRouter);
 
+
 // Start server using ViteExpress
 ViteExpress.listen(app, 3000, () => {
+
     console.log("Server running at http://localhost:3000");
+
 });
