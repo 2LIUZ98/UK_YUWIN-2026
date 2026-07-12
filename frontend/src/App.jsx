762c7da53b43301c9950eaf8831c1735ea8c0175
Zhihan Liu <2LIUZ98@solent.ac.uk>
2026-07-12 17:45:51 +0100

    FRONTNED: GET QUOTE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,34 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from "./views/Home";
 import AirportTransfer from "./views/Airport";
 import About from "./views/About";
-import DayTours from "./views/Day"; // ✅ ADD THIS
+import DayTours from "./views/Day";
+import GetQuote from "./views/Quote"; // ✅ NEW
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/airport-transfer" element={<AirportTransfer />} />
-      <Route path="/about" element={<About />} />
 
-      {/* ✅ DAY TOURS ROUTE */}
-      <Route path="/day-tours" element={<DayTours />} />
+      <Route
+        path="/airport-transfer"
+        element={<AirportTransfer />}
+      />
+
+      <Route
+        path="/about"
+        element={<About />}
+      />
+
+      <Route
+        path="/day-tours"
+        element={<DayTours />}
+      />
+
+      {/* ✅ GET QUOTE */}
+      <Route
+        path="/quote"
+        element={<GetQuote />}
+      />
     </Routes>
   );
 }
